e7f77221d82bac53654982f44ba2193e5a737b42
Jerry Wang <jerryappleid761208@gmail.com>
2019-08-19 10:56:22 -0700

    gift list

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap';
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { gifts: [] };
+  }
+
+  addGift = () => {
+    const { gifts } = this.state;
+
+    const ids = this.state.gifts.map(gift => gift.id);
+    const maxId = ids.length > 0 ? Math.max(...ids) : 0;
+
+    gifts.push({ id: maxId + 1 });
+    this.setState({ gifts });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Gift Giver</h2>
+        <Button
+          className="btn-add"
+          onClick={this.addGift}
+        >
+          Add Gift
+        </Button>
+      </div>
+    )
+  }
+}
