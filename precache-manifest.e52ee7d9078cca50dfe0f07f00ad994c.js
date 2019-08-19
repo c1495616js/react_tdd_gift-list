0daab83a8ce5a78a483ffecc4a242dccb1d4e9b9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-19 22:49:40 +0000

    Deploy c1495616js/react_tdd_gift-list to github.com/c1495616js/react_tdd_gift-list.git:gh-pages

---
+++ precache-manifest.e52ee7d9078cca50dfe0f07f00ad994c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd60952a28991193430a0079b3930264",
+    "url": "/react_tdd_gift-list/index.html"
+  },
+  {
+    "revision": "60dfd7d072bbafc393ea",
+    "url": "/react_tdd_gift-list/static/css/main.955a0061.chunk.css"
+  },
+  {
+    "revision": "b999e348c1d89610c7aa",
+    "url": "/react_tdd_gift-list/static/js/2.d201441e.chunk.js"
+  },
+  {
+    "revision": "60dfd7d072bbafc393ea",
+    "url": "/react_tdd_gift-list/static/js/main.a8f8a856.chunk.js"
+  },
+  {
+    "revision": "6296a3c0ff1f36688c8b",
+    "url": "/react_tdd_gift-list/static/js/runtime~main.ced11f00.js"
+  }
+]);
